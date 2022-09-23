74ec378d4a42d686fcfee60edcc7570600c8164b
igor02b1 <igor02b1@gmail.com>
2022-09-22 22:45:10 -0300

    exercicio-IMC

---
+++ src/script.ts
@@ -0,0 +1,13 @@
+import PromptSync = require('prompt-sync');
+const prompt = PromptSync();
+
+const Imc = (peso: number, altura: number) => {
+    return peso / Math.pow(altura, 2);
+}
+
+const peso = Number(prompt('Digite seu peso: '));
+const altura = Number(prompt('Digite sua altura: '));
+
+const imc = Imc(peso, altura);
+
+console.log(`seu IMC é: ${imc.toFixed(0)}`);
